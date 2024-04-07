e4fdd19b625d293659af83d0f4363944d4f8a67b
asaha1203 <82955361+asaha1203@users.noreply.github.com>
2024-04-07 14:21:46 -0400

    updated

--- PartII/Q5/q5_sol.py
+++ PartII/Q5/q5_sol.py
@@ -7,24 +7,31 @@ warnings.filterwarnings(
     action="ignore", category=numba.NumbaPerformanceWarning, module="numba"
 )
 
-def map_spec(a):
-    return a + 10
+def zip_spec(a, b):
+    return a + b
 
-def map_2D_test(cuda):
-    def call(out, a, size) -> None:
+def broadcast_test(cuda):
+    def call(out, a, b, size) -> None:
         local_i = cuda.threadIdx.x
         local_j = cuda.threadIdx.y
         if local_i < size and local_j < size:
-            out[local_i, local_j] = a[local_i, local_j]
+            out[local_i, local_j] = a[local_i, 0] + b[0, local_i]
 
     return call
 
 
 SIZE = 2
 out = np.zeros((SIZE, SIZE))
-a = np.arange(SIZE * SIZE).reshape((SIZE, SIZE))
+a = np.arange(SIZE).reshape(SIZE, 1)
+b = np.arange(SIZE).reshape(1, SIZE)
 problem = CudaProblem(
-    "Map 2D", map_2D_test, [a], out, [SIZE], threadsperblock=Coord(3, 3), spec=map_spec
+    "Broadcast",
+    broadcast_test,
+    [a, b],
+    out,
+    [SIZE],
+    threadsperblock=Coord(3, 3),
+    spec=zip_spec,
 )
 
 problem.check()
