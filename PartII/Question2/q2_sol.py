8cfce368c9375c273b2ecfba1968bb1974a651ea
asaha1203 <82955361+asaha1203@users.noreply.github.com>
2024-04-07 01:53:52 -0400

    did more stuff?

---
+++ PartII/Question2/q2_sol.py
@@ -0,0 +1,44 @@
+import numba
+import numpy as np
+import warnings
+from lib import CudaProblem, Coord
+
+def dot_spec(a, b):
+    return a @ b
+
+TPB = 8
+def dot_test(cuda):
+    def call(out, a, b, size):
+        shared = cuda.shared.array(TPB, numba.float32)
+
+        i = cuda.blockIdx.x * cuda.blockDim.x + cuda.threadIdx.x
+        local_i = cuda.threadIdx.x
+        
+        if i < size:
+            shared[local_i] = a[i] * b[i]
+            cuda.syncthreads()
+        if local_i == 0:
+            s = 0
+            for k in range(size):
+                s += shared[k]
+            out[0] = s
+        
+    return call
+
+
+SIZE = 8
+out = np.zeros(1)
+a = np.arange(SIZE)
+b = np.arange(SIZE)
+problem = CudaProblem(
+    "Dot",
+    dot_test,
+    [a, b],
+    out,
+    [SIZE],
+    threadsperblock=Coord(SIZE, 1),
+    blockspergrid=Coord(1, 1),
+    spec=dot_spec,
+)
+
+problem.check()
